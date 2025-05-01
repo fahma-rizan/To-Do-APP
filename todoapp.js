434ff6ae8b62d32fe8ae506d3f3584b411453bee
fahma-rizan <fahmarizan@gmail.com>
2025-05-01 22:10:42 +0530

    Add files via upload

---
+++ todoapp.js
@@ -0,0 +1,16 @@
+function addTask(){
+    const input = document.getElementById("taskInput");
+    const taskText = input.value;
+    if(taskText==="") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.onclick = () => li.remove();
+
+    li.appendChild(deleteBtn);
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
